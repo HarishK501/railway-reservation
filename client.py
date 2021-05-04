9dbdeecbe4281d0d8eb793758346ae4a6fe5092d
HarishK501 <harishcse18501@gmail.com>
2021-05-04 20:13:09 +0530

    Added train info

--- client.py
+++ client.py
@@ -7,21 +7,29 @@ client = socket.socket()
 
 client.connect((SERVER, PORT))
 # print(client.getsockname())
+options = "1. Get train info"
+print("Hi user!\n"+options)
 
 while True:
-    out_data = input("\n>>> ")
-    client.sendall(bytes(out_data, 'UTF-8'))
-    if out_data == 'bye':
-        print("Closing connection...")
-        break
-
     try:
-        in_data = client.recv(1024)
-        res = in_data.decode()
-        print("Other client :", res)
+        op = input("\n>>> ")
+        if op == "options":
+            print(options)
+        elif (op == "1"):
+            num = input("- Enter train number: ")
+            req = "getTrainInfo/"+num
+            client.sendall(bytes(req, 'UTF-8'))
+            try:
+                in_data = client.recv(1024)
+                res = in_data.decode()
+                print(res)
+            except:
+                print("Connection closed")
+                break
     except:
         print("Connection closed")
         break
+
 
 
 client.close()
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import socket
 import threading
+import pymongo
 from credentials import user
+
+def connectDB(db):
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    myDB = myclient[db]
+    return myDB
 
 
 class MyServer:
@@ -13,7 +19,8 @@ class MyServer:
         MyServer.server.bind(("127.0.0.1", port))
         print("Server started\nWaiting for client request..")
         MyServer.server.listen()
-        while True:
+        loop = True
+        while loop:
             try:
                 client, clientAddress = MyServer.server.accept()
                 MyServer.clients.append(client)
@@ -22,8 +29,7 @@ class MyServer:
             except:
                 print("Bye!")
                 break
-
-                
+            loop = int(input())
 
     class ClientThread(threading.Thread):
         def __init__(self, clientsocket, clientAddress):
@@ -33,24 +39,40 @@ class MyServer:
             print("New connection added: ", clientAddress)
 
         def run(self):
-            # self.csocket.send(bytes("Hi, This is from Server..", 'utf-8'))
+            global trains
+            # welcomeMsg = "Hi user!\n1. Get train info\n"
+            # self.csocket.send(bytes(welcomeMsg, 'utf-8'))
             msg = ''
             while True:
                 try:
-
                     data = self.csocket.recv(2048)
                     msg = data.decode()
-                    if msg == 'bye':
+                    if msg == 'exit':
                         break
-                    else:
-                        self.csocket.send(bytes(msg[::-1], 'UTF-8'))
-                        print("from client {}=>".format(self.clientAddress), msg)
+                    else:  
+                        fn, val = msg.split("/")
+                        if fn == "getTrainInfo":
+                            x = dict(trains.find_one({'number': val}))
+                            if x == None:
+                                self.csocket.send(bytes("No train found with the given number", 'utf-8'))
+                            else:
+                                res = ""
+                                for key in x.keys():
+                                    if str(key) == "_id":
+                                        continue
+                                    res += str(key).capitalize() + "\t\t: " + str(x[key]) + "\n"
+                                self.csocket.send(bytes(res, 'utf-8'))
+                            
                 except:
                     break
-                
 
             print("Client at ", self.clientAddress, " disconnected...")
             self.csocket.close()
 
+
 if __name__ == "__main__":
+    myDB = connectDB("railDB")
+    trains = myDB["trains"]
     MyServer.startServer(1247)
+    
+    # reservations = myDB["reservations"]
